cfa3b259e76b59b2fc5fa713eb528e3c418611d9
thmsrth <thoemu.roth@gmail.com>
2015-06-22 15:28:10 +0200

    Client Tests

--- src/PinnwandClient.java
+++ src/PinnwandClient.java
@@ -2,14 +2,54 @@ import java.rmi.Naming;
 
 public class PinnwandClient {
 
-    public static void main(String args[]) {
-        try {
-            PinnwandServer server = (PinnwandServer)Naming.lookup("pinnwand");
-//            String result = server
-//            System.out.println(result);
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+	public static void main(String args[]) {
+		try {
+			// Pinnwand server = (Pinnwand)Naming.lookup("pinnwand");
+			PinnwandServer server = (PinnwandServer) Naming.lookup("pinnwand");
+			
+			// Schicke Nachricht
+			if(server.putMessage("Hallo zaeme!!!")){
+				System.out.println("Nachricht erfolgreich an Pinnwand geschickt.");
+			}else{
+				System.out.println("Nachricht zu lang!");
+			}
+			
+			// Schicke 2. Nachricht
+			if(server.putMessage("Aues klaaar?")){
+				System.out.println("Nachricht erfolgreich an Pinnwand geschickt.");
+			}else{
+				System.out.println("Nachricht zu lang!");
+			}
+			
+			// Schicke zu lange Nachricht
+			if(server.putMessage("Mehr als 160 Zeicheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+					+ "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+					+ "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+					+ "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+					+ "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeen")){
+				System.out.println("Nachricht erfolgreich an Pinnwand geschickt.");
+			}else{
+				System.out.println("Nachricht zu lang!");
+			}
+			
+			// Zeige Anzahl Nachrichten
+			System.out.println("Anzahl Nachrichten: "+server.getMessageCount());
+			
+			// Gib Nachricht 0 zurueck
+			String result = server.getMessage(0);
+			System.out.println(result);
+			
+			// Liste alle Nachrichten auf
+			String[] messages = new String[20];
+			messages = server.getMessages();
+			for(String s : messages){
+				System.out.println(s);
+			}
+			
+			// System.out.println(result);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
